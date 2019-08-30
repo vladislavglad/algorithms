fc5a846fc9ec024204a58325a37ff6a2314fdef3
vladislavglad <53010628+vladislavglad@users.noreply.github.com>
2019-08-30 15:39:09 -0400

    1st algorithm
    implemented in Python

---
+++ selection-sort.py
@@ -0,0 +1,25 @@
+import random
+
+#selection sort algorithm is an O(n^2) alg.
+def selection_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j = j - 1
+        
+        arr[j+1] = key
+
+#generate a random list
+arr = []
+for i in range(10):
+    arr.append(random.randint(0,99))
+
+#print this list before sorting.
+print(f'before: {arr}')
+
+#print this list after sorting.
+selection_sort(arr)
+print(f'after: {arr}')
